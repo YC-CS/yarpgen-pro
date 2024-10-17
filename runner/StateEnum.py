224406a284005f5de9415dc67258ca9918b56118
YC <YC1054073667@outlook.com>
2024-10-17 18:06:58 +0800

    refactor(/runner): 增加测试脚本
    在/runner文件夹下增加编译器测试脚本，在脚本中输入各参数后，即可进行自动测试

---
+++ runner/StateEnum.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+def state_to_str(state: int):
+    if state == State.COMPILE_SUCC:
+        return 'COMPILE_SUCC'
+    elif state == State.COMPILE_TIMEOUT:
+        return 'COMPILE_TIMEOUT'
+    elif state == State.COMPILE_CRASH:
+        return 'COMPILE_CRASH'
+    elif state == State.EXECUTION_SUCC:
+        return 'EXECUTION_SUCC'
+    elif state == State.EXECUTION_TIMEOUT:
+        return 'EXECUTION_TIMEOUT'
+    elif state == State.EXECUTION_CRASH:
+        return 'EXECUTION_CRASH'
+    else:
+        raise ValueError('State should in [1, 6] ' + state)
+
+
+class State(Enum):
+    COMPILE_SUCC = 1
+    COMPILE_TIMEOUT = 2
+    COMPILE_CRASH = 3
+    EXECUTION_SUCC = 4
+    EXECUTION_TIMEOUT = 5
+    EXECUTION_CRASH = 6
+
+
+class Language(int):
+    c = 1
+    cpp = 2
